b4dcb02e337b43d8bb0044ba010ac9a45e709e69
Nischithagowda <nischishekar2@gmail.com>
2022-11-02 18:02:28 +0530

    Adding production URL

--- app/src/main/java/com/bmc/suchane_svamitva/api/APIClient_Suchane.java
+++ app/src/main/java/com/bmc/suchane_svamitva/api/APIClient_Suchane.java
@@ -2,14 +2,10 @@ package com.bmc.suchane_svamitva.api;
 
 import android.content.Context;
 
-import com.bmc.suchane_svamitva.BuildConfig;
 import com.google.gson.GsonBuilder;
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
-import java.util.concurrent.TimeUnit;
-
 import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -20,23 +16,26 @@ public class APIClient_Suchane {
 
     public static Retrofit getClient(Context context, String url) {
         if (retrofit == null) {
-            OkHttpClient.Builder client = new OkHttpClient
-                    .Builder();
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-            if (BuildConfig.DEBUG) {
-                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-            } else {
-                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-            }
-            client.connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
-                    .addInterceptor(logging)
-                    .authenticator(new AccessTokenAuthenticator(context))
-            ;
+
+            OkHttpClient client = UnsafeOkHttpClient.getUnsafeOkHttpClient(context);
+
+//            OkHttpClient.Builder client = new OkHttpClient
+//                    .Builder();
+//            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+//            if (BuildConfig.DEBUG) {
+//                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+//            } else {
+//                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+//            }
+//            client.connectTimeout(30, TimeUnit.SECONDS)
+//                    .readTimeout(30, TimeUnit.SECONDS)
+//                    .writeTimeout(30, TimeUnit.SECONDS)
+//                    .addInterceptor(logging)
+//                    .authenticator(new AccessTokenAuthenticator(context))
+//            ;
             retrofit = new Retrofit.Builder()
                     .baseUrl(url)
-                    .client(client.build())
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
                             .setLenient()
                             .create()))
@@ -49,22 +48,24 @@ public class APIClient_Suchane {
 
     public static Retrofit getClientWithoutToken(String url) {
         if (retrofitWithoutToken == null) {
-            OkHttpClient.Builder client = new OkHttpClient
-                    .Builder();
-            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-            if (BuildConfig.DEBUG) {
-                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-            } else {
-                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
-            }
-            client.connectTimeout(30, TimeUnit.SECONDS)
-                    .readTimeout(30, TimeUnit.SECONDS)
-                    .writeTimeout(30, TimeUnit.SECONDS)
-                    .addInterceptor(logging);
+
+            OkHttpClient client = UnsafeOkHttpClientForToken.getUnsafeOkHttpClient();
+//            OkHttpClient.Builder client = new OkHttpClient
+//                    .Builder();
+//            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+//            if (BuildConfig.DEBUG) {
+//                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+//            } else {
+//                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+//            }
+//            client.connectTimeout(30, TimeUnit.SECONDS)
+//                    .readTimeout(30, TimeUnit.SECONDS)
+//                    .writeTimeout(30, TimeUnit.SECONDS)
+//                    .addInterceptor(logging);
 
             retrofitWithoutToken = new Retrofit.Builder()
                     .baseUrl(url)
-                    .client(client.build())
+                    .client(client)
                     .addConverterFactory(GsonConverterFactory.create(new GsonBuilder()
                             .setLenient()
                             .create()))
